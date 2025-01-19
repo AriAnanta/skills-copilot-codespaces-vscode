9a31f6d450ee9a62602ce94854233af6b00a3326
Ari Ananta <147940474+AriAnanta@users.noreply.github.com>
2025-01-19 14:35:36 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,35 @@
 // create web server
-app.get('/comments', (req, res) => {
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var comments = require('./comments.json');
+
+// create server
+app.listen(3000, function() {
+  console.log('Server running on http://localhost:3000');
+});
+
+// Middleware
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/comments', function(req, res) {
   res.json(comments);
 });
 
-app.post('/comments', (req, res) => {
-  const newComment = {
-    id: Date.now(),
+app.post('/comments', function(req, res) {
+  comments.push(req.body);
+  fs.writeFile('comments.json', JSON.stringify(comments, null, 2), function(err) {
+    if (err) {
+      return res.sendStatus(500);
+    }
+    res.sendStatus(201);
+  });
+});
