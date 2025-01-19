7a86774730fe2987e766ba5a341e511af26c713f
Ari Ananta <147940474+AriAnanta@users.noreply.github.com>
2025-01-19 14:37:51 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,35 +1,14 @@
 // create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var comments = require('./comments.json');
+// create a route for comments
+// send the comments data to the client
+const express = require('express');
+const app = express();
+const comments = require('./comments-data');
 
-// create server
-app.listen(3000, function() {
-  console.log('Server running on http://localhost:3000');
-});
-
-// Middleware
-app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Routes
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/comments', function(req, res) {
+app.get('/comments', (req, res) => {
   res.json(comments);
 });
 
-app.post('/comments', function(req, res) {
-  comments.push(req.body);
-  fs.writeFile('comments.json', JSON.stringify(comments, null, 2), function(err) {
-    if (err) {
-      return res.sendStatus(500);
-    }
-    res.sendStatus(201);
-  });
+app.listen(4001, () => {
+  console.log('Server is listening on port 4001');
 });
